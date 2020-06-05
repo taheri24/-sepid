e77c37e84b86b05116cc1ba7ed9a7e06fce2b960
Majid <majid.taheri@yandex.com>
2020-06-05 17:03:26 +0430

    change config of rollup for multiple output

--- rollup.config.ts
+++ rollup.config.ts
@@ -2,43 +2,46 @@ import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import sourceMaps from 'rollup-plugin-sourcemaps'
 import react from 'react';
- 
+import { RollupDirOptions } from 'rollup';
 import typescript from 'rollup-plugin-typescript2'
 import json from 'rollup-plugin-json'
-
+import path from 'path';
 const pkg = require('./package.json')
 
 const libraryName = pkg.name;
+const inputFiles=[`src/index.ts`, 'src/store-presentation.tsx'];
+export default inputFiles.map(input => {
+  const {name}=path.parse(input);
+  return {
+    input,
+    output: [
+      { file:name=='index' ?  pkg.main : pkg.main.replace('safe-hook',name), 
+        name:name=='index' ? libraryName : name, format: 'umd', sourcemap: false },
+      { file: name=='index'? pkg.module : pkg.module.replace('safe-hook',name),
+       format: 'es', 
+       sourcemap: false },
+    ],
+    // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
+    external: [],
+    plugins: [
+      // Allow json resolution
+      json(),
+      // Compile TypeScript files
+      typescript({ useTsconfigDeclarationDir: true }),
+      // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
+      commonjs({
+        include: 'node_modules/**',
+        namedExports: {
+          react: Object.keys(react),
+        }
+      }),
+      // Allow node_modules resolution, so you can use 'external' to control
+      // which external modules to include in the bundle
+      // https://github.com/rollup/rollup-plugin-node-resolve#usage
+      resolve(),
 
-export default {
-  input: `src/index.ts`,
-  output: [
-    { file: pkg.main, name:   libraryName , format: 'umd', sourcemap: false },
-    { file: pkg.module, format: 'es', sourcemap: false },
-  ],
-  // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
-  external: [],
-  watch: {
-    include: 'src/**',
-  },
-  plugins: [
-    // Allow json resolution
-    json(),
-    // Compile TypeScript files
-    typescript({ useTsconfigDeclarationDir: true }),
-    // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
-    commonjs({
-      include: 'node_modules/**',
-      namedExports: {
-        react: Object.keys(react),
-       }
-    }),
-    // Allow node_modules resolution, so you can use 'external' to control
-    // which external modules to include in the bundle
-    // https://github.com/rollup/rollup-plugin-node-resolve#usage
-    resolve(),
-
-    // Resolve source maps to the original source
-    sourceMaps(),
-  ],
-}
+      // Resolve source maps to the original source
+      sourceMaps(),
+    ],
+  }
+}) //as RollupDirOptions;
