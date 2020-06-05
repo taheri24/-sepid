58f4ef6eb77a89941efc8b088b68396015044881
Majid <majid.taheri@yandex.com>
2020-06-05 17:03:08 +0430

    first version of StorePresentation(w/o impl)

---
+++ src/store-presentation.tsx
@@ -0,0 +1,11 @@
+import * as React from "react"
+
+export const  StorePresentation:React.FC<StorePresentation.Props>=p=>{
+    return <>
+    </>
+}
+export namespace StorePresentation{
+    export interface Props{
+        store:any;
+    }
+}
